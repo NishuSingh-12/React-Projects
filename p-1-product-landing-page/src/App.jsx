1a850afac9996b0889ac1c1e165e3199d8c16216
Nishu singh <nishu.frontend@gmail.com>
2026-04-09 20:52:02 +0530

    Add a footer section with styling

--- p-1-product-landing-page/src/App.jsx
+++ p-1-product-landing-page/src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import "./App.css";
 import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="main-container">
       <Navbar />
       <Hero />
+      <Footer />
     </div>
   );
 }
---
+++ p-1-product-landing-page/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <div className="shop-container">
+      <a href="#">
+        <img src="/images/shops.png" alt="amzone and flipcart" />
+      </a>
+    </div>
+  );
+}
+export default Footer;
