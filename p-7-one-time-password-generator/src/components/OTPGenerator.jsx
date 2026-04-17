4433e325e0b5a28f4dcca75ab881c7c9976ce6ad
Nishu singh <nishu.frontend@gmail.com>
2026-04-17 17:26:14 +0530

    Add a generateOTP function

--- p-7-one-time-password-generator/src/components/OTPGenerator.jsx
+++ p-7-one-time-password-generator/src/components/OTPGenerator.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
+
 function OTPGenerator() {
+  const [otp, setOtp] = useState("");
+  const [timeLeft, setTimeLeft] = useState(0);
+  const generateOTP = () => {
+    const newOTP = Math.floor(100000 + Math.random() * 900000).toString();
+    setOtp(newOTP);
+    setTimeLeft(5);
+  };
   return (
     <div className="container">
       <h1>OTP Generator</h1>
-      <p id="sub-heading">Click 'Generate OTP' to get a code</p>
+      <p id="sub-heading">{otp ? otp : "Click 'Generate OTP' to get a code"}</p>
       <p id="timer">Expire in:</p>
-      <button>Generate OTP</button>
+      <button onClick={generateOTP}>Generate OTP</button>
     </div>
   );
 }
